3bf608ab9b13a3c0472e2db20b496924b6618174
Anna Toth <toth.annelise@gmail.com>
2023-02-27 15:52:44 +0100

    fix debugger

--- FactoryApi/Models/Order.cs
+++ FactoryApi/Models/Order.cs
@@ -5,6 +5,6 @@ public class Order
     public long Id { get; set; }
     public long StationId { get; set; }
     public State State { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int Duration { get; set; }
 }
