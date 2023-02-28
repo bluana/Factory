40bf716abb84f5bfc7d12fc2c75860fc5e445ef0
Anna Toth <toth.annelise@gmail.com>
2023-02-28 04:43:01 +0100

    add routing

--- stations-fe/src/app/app-routing.module.ts
+++ stations-fe/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryViewComponent } from './components/history-view/history-view.component';
+import { StationDetailViewComponent } from './components/station-detail-view/station-detail-view.component';
+import { StationsViewComponent } from './components/stations-view/stations-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/stations', pathMatch: 'full' },
+  { path: 'stations', component: StationsViewComponent },
+  { path: 'stations/:id', component: StationDetailViewComponent },
+  { path: 'history', component: HistoryViewComponent },
+  { path: '**', redirectTo: '/stations' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ stations-fe/src/app/common/constants/http-urls.ts
@@ -0,0 +1,8 @@
+export const httpUrls = {
+  api: "http://localhost:5225/api",
+};
+
+export const urlExtensions = {
+  stations: "Stations",
+  history: 'History',
+}
