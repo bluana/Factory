794d330ff8026c5a99e856b1fe99db41e4ea187e
Anna Toth <toth.annelise@gmail.com>
2023-02-28 04:40:54 +0100

    add angular material

--- stations-fe/src/app/app.module.ts
+++ stations-fe/src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StationsViewComponent } from './components/stations-view/stations-view.component';
+import { StationDetailViewComponent } from './components/station-detail-view/station-detail-view.component';
+import { HistoryViewComponent } from './components/history-view/history-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StationsViewComponent,
+    StationDetailViewComponent,
+    HistoryViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
